aed9e90e5c12d59fa3cc19555c95b05dd6c70415
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-10-12 18:37:02 -0300

    ✨ feat: login controller

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,10 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { UserModule } from '../user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
   imports: [ConfigModule.forRoot(), UserModule, PassportModule, JwtModule.register({ privateKey: process.env.JWT_SECRET })],
-  controllers: [],
-  providers: [],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
