6bcab16c4589d99ac055e5535ab398ee588347d9
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-10-12 18:51:18 -0300

    ✨ feat: User Roles

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { LocalGuard } from './guard/local.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @UseGuards(LocalGuard)
   login(@Body() loginDto: LoginDto) {
     return this.authService.validate(loginDto);
   }
---
+++ src/app/auth/roles/roles.ts
@@ -0,0 +1,4 @@
+import { UserType } from '../../user/enum/user-type.enum';
+
+export const UserRole = [UserType.Normal, UserType.Admin];
+export const AdminRole = [UserType.Admin];
