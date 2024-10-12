1e502edbd3710e3cad82b3cbdd8e6c0c525d8c99
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-10-12 19:02:56 -0300

    ✨ feat: use APP_GUARD

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,10 +6,11 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategy/jwt.strategy';
+import { LocalStrategy } from './strategy/local.strategy';
 
 @Module({
   imports: [ConfigModule.forRoot(), UserModule, PassportModule, JwtModule.register({ privateKey: process.env.JWT_SECRET })],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, JwtStrategy, LocalStrategy],
 })
 export class AuthModule {}
