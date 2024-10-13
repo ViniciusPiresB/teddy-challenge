5eff9b182df61d6da48b53e5ab4066569693d75f
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-10-12 23:31:13 -0300

    ✨ feat: swagger on controllers

--- src/app/user/user.controller.ts
+++ src/app/user/user.controller.ts
@@ -5,22 +5,44 @@ import { Roles } from '../auth/decorator/roles.decorator';
 import { AdminRole, UserRole } from '../auth/roles/roles';
 import { GetUser } from '../decorator/get-user.decorator';
 import { JwtPayload } from '../auth/dto/jwt-payload.dto';
+import { ApiOperation, ApiTags, ApiCreatedResponse, ApiConflictResponse, ApiForbiddenResponse, ApiBearerAuth, ApiOkResponse, ApiNotFoundResponse } from '@nestjs/swagger';
 
 @Controller('user')
+@ApiTags('User')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiOperation({ summary: 'Creates a user.' })
+  @ApiCreatedResponse({ description: 'Created Successfully.' })
+  @ApiConflictResponse({ description: 'User already exist.' })
+  @ApiForbiddenResponse({
+    description: 'Missing token or not enough permission.',
+  })
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
+  @ApiOperation({ summary: 'Find one user by email.' })
+  @ApiOkResponse({ description: 'User Found.' })
+  @ApiNotFoundResponse({ description: 'User not found' })
+  @ApiForbiddenResponse({
+    description: 'Missing token or not enough permission.',
+  })
+  @ApiBearerAuth()
   @Roles(...AdminRole)
   @Get(':email')
   findByEmail(@Param('email') email: string) {
     return this.userService.findByEmail(email);
   }
 
+  @ApiOperation({ summary: 'Delete your user.' })
+  @ApiOkResponse({ description: 'User deleted.' })
+  @ApiNotFoundResponse({ description: 'User not found' })
+  @ApiForbiddenResponse({
+    description: 'Missing token or not enough permission.',
+  })
+  @ApiBearerAuth()
   @Roles(...UserRole)
   @Delete()
   deleteAccount(@GetUser() activeUser: JwtPayload) {
