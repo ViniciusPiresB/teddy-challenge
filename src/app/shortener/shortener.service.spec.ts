60f035d48ec15090021a87e667400c40641cb714
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-10-13 15:21:47 -0300

    🧪 test: create short url with user

--- src/app/shortener/shortener.service.spec.ts
+++ src/app/shortener/shortener.service.spec.ts
@@ -1,23 +1,29 @@
 import { PrismaService } from '../../database/prisma.service';
 import { ShortenerService } from './shortener.service';
-import { ShortUrls, Status, User } from '@prisma/client';
+import { ShortUrls, Status } from '@prisma/client';
 import { Test, TestingModule } from '@nestjs/testing';
+import { JwtPayload } from '../auth/dto/jwt-payload.dto';
+import * as nanoid from 'nanoid';
+
+jest.mock('nanoid', () => ({
+  nanoid: jest.fn(),
+}));
 
 describe('ShortenerService', () => {
   let shortenerService: ShortenerService;
   let prismaService: PrismaService;
 
-  const fakeUser: User = {
+  const fakeUser: JwtPayload = {
     name: 'Test User 1',
-    password: 'pass123',
     id: 'a3718843-5456-4482-9c97-a20f78cbd44e',
     email: 'test@user.com',
     username: 'testuser1',
     typeUser: 1,
     status: Status.ACTIVE,
-    createdAt: new Date(),
-    updatedAt: new Date(),
+    createdAt: new Date().toString(),
+    updatedAt: new Date().toString(),
     deletedAt: null,
+    iat: Date.now(),
   };
 
   const fakeShortUrlsFromUser: ShortUrls[] = [
@@ -65,5 +71,26 @@ describe('ShortenerService', () => {
   it('Should be defined', () => {
     expect(shortenerService).toBeDefined();
     expect(prismaService).toBeDefined();
+  });
+
+  describe('createShortUrl', () => {
+    it('Should create a short URL successfully with logged user', async () => {
+      const longUrl = 'http://example.com';
+      (nanoid.nanoid as jest.Mock).mockReturnValueOnce(fakeShortUrlsFromUser[0].shortUrl);
+
+      const result = await shortenerService.createShortUrl(longUrl, fakeUser);
+
+      expect(result).toEqual({
+        shortenedUrl: `${process.env.BASE_URL}/${fakeShortUrlsFromUser[0].shortUrl}`,
+      });
+      expect(prismaService.shortUrls.create).toHaveBeenCalledWith({
+        data: {
+          longUrl,
+          shortUrl: fakeShortUrlsFromUser[0].shortUrl,
+          userId: fakeUser.id,
+        },
+      });
+      expect(prismaService.shortUrls.create).toHaveBeenCalledTimes(1);
+    });
   });
 });
