73e19a2ce7f82188f73993c2d0d2acc167a77f3f
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-10-13 11:58:19 -0300

    🧪 test: auth return token

---
+++ src/app/auth/auth.service.spec.ts
@@ -0,0 +1,68 @@
+import { JwtService } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from '../user/user.service';
+import { Status, User } from '@prisma/client';
+import { validatePassword } from '../utils/validate-password';
+import { LoginDto } from './dto/login.dto';
+
+jest.mock('../utils/validate-password');
+
+describe('AuthService', () => {
+  let authService: AuthService;
+  let userService: UserService;
+  let jwtService: JwtService;
+
+  const fakeUser: User = {
+    name: 'Test User 1',
+    password: 'pass123',
+    id: 'a3718843-5456-4482-9c97-a20f78cbd44e',
+    email: 'test@user.com',
+    username: 'testuser1',
+    typeUser: 1,
+    status: Status.ACTIVE,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null,
+  };
+
+  const mockUserService = {
+    findByEmail: jest.fn().mockResolvedValue(fakeUser),
+  };
+
+  const mockJwtService = {
+    sign: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [AuthService, { provide: UserService, useValue: mockUserService }, { provide: JwtService, useValue: mockJwtService }],
+    }).compile();
+
+    authService = module.get<AuthService>(AuthService);
+    userService = module.get<UserService>(UserService);
+    jwtService = module.get<JwtService>(JwtService);
+  });
+
+  it('Should be defined', () => {
+    expect(authService).toBeDefined();
+    expect(userService).toBeDefined();
+    expect(jwtService).toBeDefined();
+  });
+
+  describe('validate', () => {
+    const loginDto: LoginDto = {
+      email: 'test@user.com',
+      password: 'pass123',
+    };
+
+    it('Should return a accessToken if user is valid', async () => {
+      (validatePassword as jest.Mock).mockResolvedValueOnce(true);
+      mockJwtService.sign.mockReturnValueOnce('fake token');
+
+      const result = await authService.validate(loginDto);
+
+      expect(result).toEqual({ accessToken: 'fake token' });
+    });
+  });
+});
