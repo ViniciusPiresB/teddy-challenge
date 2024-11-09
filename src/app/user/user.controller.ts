d4d307775bb6edcab451da5589149440e9e345d1
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-11-09 15:41:04 -0300

    refactor: replace controller

--- src/app/user/user.controller.ts
+++ src/app/user/user.controller.ts
@@ -1,33 +1,44 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { ApiOperation, ApiOkResponse, ApiNotFoundResponse, ApiBearerAuth, ApiCreatedResponse, ApiConflictResponse, ApiForbiddenResponse } from '@nestjs/swagger';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiOperation({ summary: 'Creates a user.' })
+  @ApiCreatedResponse({ description: 'Created Successfully.' })
+  @ApiConflictResponse({ description: 'User already exist.' })
+  @ApiForbiddenResponse({
+    description: 'Missing token or not enough permission.',
+  })
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
-  @Get()
-  findAll() {
-    return this.userService.findAll();
+  @ApiOperation({ summary: 'Find one user by email.' })
+  @ApiOkResponse({ description: 'User Found.' })
+  @ApiNotFoundResponse({ description: 'User not found' })
+  @ApiForbiddenResponse({
+    description: 'Missing token or not enough permission.',
+  })
+  @ApiBearerAuth()
+  @Get(':email')
+  findByEmail(@Param('email') email: string) {
+    return this.userService.findByEmail(email);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string) {
-    return this.userService.update(+id);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  @ApiOperation({ summary: 'Delete your user.' })
+  @ApiOkResponse({ description: 'User deleted.' })
+  @ApiNotFoundResponse({ description: 'User not found' })
+  @ApiForbiddenResponse({
+    description: 'Missing token or not enough permission.',
+  })
+  @ApiBearerAuth()
+  @Delete()
+  deleteAccount() {
+    return this.userService.delete('');
   }
 }
