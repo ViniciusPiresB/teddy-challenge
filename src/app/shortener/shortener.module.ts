6f793eba24771268a7b26673ccf66c94f87ce766
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-11-10 14:29:46 -0300

    feat: setup cache module

--- src/app/shortener/shortener.module.ts
+++ src/app/shortener/shortener.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ShortenerService } from './shortener.service';
 import { ShortenerController } from './shortener.controller';
 import { PrismaService } from 'src/database/prisma.service';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
+  imports: [CacheModule.register({ ttl: 300000, max: 100 })],
   controllers: [ShortenerController],
   providers: [ShortenerService, PrismaService],
 })
