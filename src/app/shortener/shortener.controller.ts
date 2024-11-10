60108142cef4f02a11f494b30aba208b0809e5a5
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-11-09 23:59:13 -0300

    feat: Link url to user

--- src/app/shortener/shortener.controller.ts
+++ src/app/shortener/shortener.controller.ts
@@ -1,15 +1,22 @@
 import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
 import { ShortenerService } from './shortener.service';
 import { CreateShortDto } from './dto/create-short.dto';
-import { ApiNotFoundResponse, ApiOperation } from '@nestjs/swagger';
+import { ApiBadRequestResponse, ApiBearerAuth, ApiCreatedResponse, ApiNotFoundResponse, ApiOperation } from '@nestjs/swagger';
+import { GetUser } from '../decorator/get-user.decorator';
+import { JwtPayload } from '../auth/dto/jwt-payload.dto';
 
 @Controller('shortener')
 export class ShortenerController {
   constructor(private readonly shortenerService: ShortenerService) {}
 
+  @ApiOperation({ summary: 'Creates an url shortened.' })
+  @ApiCreatedResponse({ description: 'URL created successfully.' })
+  @ApiBadRequestResponse({ description: 'Url Empty.' })
+  @ApiBadRequestResponse({ description: 'Url must be a string.' })
+  @ApiBearerAuth()
   @Post()
-  create(@Body() createShortDto: CreateShortDto) {
-    return this.shortenerService.createShortUrl(createShortDto.url);
+  create(@Body() createShortDto: CreateShortDto, @GetUser() user: JwtPayload) {
+    return this.shortenerService.createShortUrl(createShortDto.url, user);
   }
 
   @ApiOperation({ summary: 'Redirect the short url to url registered.' })
